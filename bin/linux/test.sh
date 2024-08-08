1bedc87a867bf56535d8241bcd2466d367f86b7f
ellyx13 <pltkiet@gmail.com>
2024-08-08 22:23:45 +0700

    feat(bin): docker executable file for windows, linux

---
+++ bin/linux/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker compose -f docker-compose-test.yml build
+docker compose -f docker-compose-test.yml up --abort-on-container-exit
+docker compose -f docker-compose-test.yml down -v

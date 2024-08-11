6f17cbba9df83b30f663e7d595bf2accd933b9fd
ellyx13 <pltkiet@gmail.com>
2024-08-11 07:25:49 +0000

    feat(auth): add bearer token in openapi schemas

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 import sys
 
 from config import settings
+from exceptions import CustomException
 from fastapi import FastAPI, Request, Response
+from fastapi.openapi.utils import get_openapi
 from fastapi.responses import JSONResponse
+from fastapi.routing import APIRoute
 from loguru import logger
 from routers import api_routers
-from exceptions import CustomException
 
 app = FastAPI(
     title="FastAPI Base Project",
@@ -45,3 +47,29 @@ async def standard_exception_handler(request: Request, exc: CustomException):
     if exc.status in [304, 204]:
         return Response(status_code=exc.status)
     return JSONResponse(status_code=exc.status, content={"type": exc.type, "title": exc.title, "status": exc.status, "detail": exc.detail})
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(title="FastAPI Base Project", version="0.0.1", routes=app.routes)
+    openapi_schema["components"]["securitySchemes"] = {
+        "Bearer Auth": {"type": "apiKey", "in": "header", "name": "Authorization", "description": "Enter: **'Bearer &lt;JWT&gt;'**, where JWT is the access token"}
+    }
+
+    # Get all routes where jwt_optional() or jwt_required
+    api_router = [route for route in app.routes if isinstance(route, APIRoute)]
+
+    for route in api_router:
+        path = getattr(route, "path")
+        methods = [method.lower() for method in getattr(route, "methods")]
+
+        for method in methods:
+            openapi_schema["paths"][path][method]["security"] = [{"Bearer Auth": []}]
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi

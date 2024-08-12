add4754adc76088aee01abe6d1b44d5bdcddc84c
ellyx13 <pltkiet@gmail.com>
2024-08-12 22:27:39 +0700

    docs(authen): docstring class and method

--- app/auth/decoractor.py
+++ app/auth/decoractor.py
@@ -7,6 +7,18 @@ from .exceptions import ErrorCode as AuthErrorCode
 
 
 class access_control:
+    """
+    A decorator class for controlling access to methods based on user roles.
+
+    This class is used to restrict access to certain api, allowing only users with specific roles (e.g., admin) to execute them.
+
+    Args:
+        admin (bool, optional): If set to True, the decorated method will only be accessible to admin users. Defaults to False.
+
+    Attributes:
+        admin (bool): Indicates whether the method should be restricted to admin users.
+    """
+
     def __init__(cls, admin: bool = False) -> None:
         cls.admin = admin
 
--- app/auth/services.py
+++ app/auth/services.py
@@ -13,7 +13,17 @@ class AuthenticationServices(BaseServices):
     def __init__(self, service_name: str, crud: BaseCRUD = None) -> None:
         super().__init__(service_name, crud)
 
-    async def create_access_token(self, user_id: str, user_type: str):
+    async def create_access_token(self, user_id: str, user_type: str) -> dict:
+        """
+        Creates a JWT access token for the specified user.
+
+        Args:
+            user_id (str): The ID of the user for whom the token is being created.
+            user_type (str): The type of the user (e.g., admin, customer).
+
+        Returns:
+            str: The encoded JWT access token.
+        """
         expire = calculator.add_days_to_datetime(days=settings.access_token_expire_day)
         expire_str = converter.convert_datetime_to_str(datetime_obj=expire)
         to_encode = {"user_id": user_id, "user_type": user_type, "expire": expire_str}
@@ -21,6 +31,16 @@ class AuthenticationServices(BaseServices):
         return encoded_jwt
 
     async def validate_access_token(self, token: str) -> bool:
+        """
+        Validates a JWT access token.
+
+        Args:
+            token (str): The JWT access token to be validated.
+
+        Returns:
+            bool: True if the token is valid and not expired, False otherwise.
+
+        """
         try:
             token = token.split(" ")[1]
             payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
@@ -34,6 +54,16 @@ class AuthenticationServices(BaseServices):
             return False
 
     async def check_public_api(self, request: Request) -> bool:
+        """
+        Checks if the API being accessed is a public API.
+
+        Args:
+            request (Request): The FastAPI request object.
+
+        Returns:
+            bool: True if the API path is listed as a public API, False otherwise.
+
+        """
         api_path = request.url.path
         if api_path in PUBLIC_APIS:
             return True

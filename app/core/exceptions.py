f4a6bc88b222a640228cf48216e6dbd1028d5894
ellyx13 <pltkiet@gmail.com>
2024-08-13 00:48:16 +0700

    test(users): unit test

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -4,11 +4,11 @@ from app.exceptions import CustomException
 class ErrorCode:
     @staticmethod
     def NotFound(service_name: str, item: str):
-        return CustomException(type=f"{service_name}/warning/not-found", status=404, title="Not Found.", detail=f"{service_name.capitalize()} with {item} could not be found.")
+        return CustomException(type=f"{service_name}/warning/not-found", status=404, title="Not found.", detail=f"{service_name.capitalize()} with {item} could not be found.")
 
     @staticmethod
     def NotModified(service_name: str):
-        return CustomException(type=f"{service_name}/warning/not-modified", status=304, title="Not Modified.", detail="Content has not changed since the last request. No update needed.")
+        return CustomException(type=f"{service_name}/warning/not-modified", status=304, title="Not modified.", detail="Content has not changed since the last request. No update needed.")
 
     @staticmethod
     def Conflict(service_name: str, item: str):

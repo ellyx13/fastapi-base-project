73dfc58d222f781dbd1c3df5b383d66fe67f1bd1
ellyx13 <pltkiet@gmail.com>
2025-04-06 06:53:39 +0000

    fix(tests): update user ID retrieval in tests and assert status codes for unauthorized access

--- tests/users/users_test.py
+++ tests/users/users_test.py
@@ -31,18 +31,24 @@ async def test_get_me(client: AsyncClient):
 @pytest.mark.asyncio(scope="session")
 async def test_user_detail(client: AsyncClient):
     user = await test_user_login(client)
-    user_id = user["_id"]
+    user_id = user["id"]
     headers = {"Authorization": f"Bearer {user['access_token']}"}
     response = await client.get(f"v1/users/{user_id}", headers=headers)
+    assert response.status_code == 403
+
+    response = await client.get("v1/users/me", headers=headers)
     assert response.status_code == 200
 
 
 @pytest.mark.asyncio(scope="session")
 async def test_user_edit(client: AsyncClient):
     user = await test_user_login(client)
-    user_id = user["_id"]
+    user_id = user["id"]
     headers = {"Authorization": f"Bearer {user['access_token']}"}
     payload = {"fullname": "new_name"}
     response = await client.put(f"v1/users/{user_id}", headers=headers, json=payload)
+    assert response.status_code == 403
+
+    response = await client.put("v1/users/me", headers=headers, json=payload)
     assert response.status_code == 200
     assert response.json()["fullname"] == "new_name"

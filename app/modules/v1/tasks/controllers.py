8d71f78b2663804236eb539a45a106089f0888b8
ellyx13 <pltkiet@gmail.com>
2025-04-08 01:02:05 +0000

    fix(controllers): ensure current user is validated before task creation and editing

--- app/modules/v1/tasks/controllers.py
+++ app/modules/v1/tasks/controllers.py
@@ -1,5 +1,6 @@
 from core.controllers import BaseControllers
 from core.schemas import CommonsDependencies
+from users.controllers import user_controllers
 
 from . import schemas
 from .models import Tasks
@@ -11,9 +12,11 @@ class TaskControllers(BaseControllers[TaskServices]):
         super().__init__(controller_name="tasks", service=task_services)
 
     async def create(self, data: schemas.CreateRequest, commons: CommonsDependencies) -> Tasks:
+        await user_controllers.get_by_id(_id=commons.current_user, commons=commons)
         return await self.service.create(data=data, commons=commons)
 
     async def edit(self, _id: str, data: schemas.EditRequest, commons: CommonsDependencies) -> Tasks:
+        await user_controllers.get_by_id(_id=commons.current_user, commons=commons)
         await self.get_by_id(_id=_id, commons=commons)
         return await self.service.edit(_id=_id, data=data, commons=commons)
 
